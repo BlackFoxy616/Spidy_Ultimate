2d7ac3639f8aa77a286364ccc856f9b786d51e95
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-02-17 09:30:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,17 +22,16 @@ async def progress(current, total):
 async def start_command(client,message):
      now=datetime.now()
      crtda = now.strftime('%m/%d/%y)
+     print(crtda)
      channel_id = message.chat.id
      await app.send_message(channel_id,"Updating.....")
      filec = open("links.txt","r")
      read=csv.reader(filec)
      for link in read:
-        os.system("""yt-dlp --downloader aria2c -I 1:1 -o '%(uploader)s/%(title)s.%(ext)s' --download-archive dllinks.txt -f '(480[vcodec~="^((he|a)vc|h26[45])"]+ba) / (480+ba/b)' --embed-thumbnail --embed-metadata """ + link[0])
+        os.system("""yt-dlp --downloader aria2c -I 1:1 -o 'crtda' --download-archive dllinks.txt -f '(480[vcodec~="^((he|a)vc|h26[45])"]+ba) / (480+ba/b)' --embed-thumbnail --embed-metadata """ + link[0])
     
-     for  filename in os.listdir():
-          if os.path.isdir(filename):
-             for names in os.listdir(filename):
-               if names.endswith(".mp4") :
+     for  filename in os.listdir(crtda):
+               if filename.endswith(".mp4") :
                     await app.send_document(-1001737315050, document=filename+'/'+names,caption=names,progress=progress)
 
 

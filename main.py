ebeb9813d85ededbfcd0abe99b4f73970d3b892a
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-02-17 11:58:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def start_command(client,message):
      read=csv.reader(filec)
 
      for link in read:
-        os.system("""./yt-dlp --downloader aria2c -I 1:2 -o '%(title)s.%(ext)s' --download-archive dllinks.txt -f '(mp4)[height=?240]' --write-thumbnail --embed-metadata """ + link[0])
+        os.system("""yt-dlp --downloader aria2c -I 1:2 -o '%(title)s.%(ext)s' --download-archive dllinks.txt -f '(mp4)[height=?240]' --write-thumbnail --embed-metadata """ + link[0])
      for  filename in os.listdir():
                print(filename)
                if filename.endswith(".mp4") :

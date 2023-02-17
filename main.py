9dd1073a1c3fa5e87f13fa5eac6b87b47997c1a0
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-02-17 13:20:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ app = Client(
 async def progress(current, total):
     print(f"{current * 100 / total:.1f}%")
 
-@app.on_message(filters.command("update"))
+
+
+
+@app.on_message(filters.command("updateall"))
 async def start_command(client,message):
      channel_id = message.chat.id
      await app.send_message(channel_id,"Updating.....")
@@ -24,6 +27,19 @@ async def start_command(client,message):
 
      for link in read:
         os.system("""yt-dlp --downloader aria2c -I 1:2 -o '%(title)s.%(ext)s' --download-archive dllinks.txt -f '(mp4)[height=?240]' --write-thumbnail --embed-metadata """ + link[0])
+     for  filename in os.listdir():
+               print(filename)
+               if filename.endswith(".mp4") :
+                    await app.send_video(-1001737315050, video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"),progress=progress)
+
+
+
+
+@app.on_message(filters.command("update"))
+async def start_command(client,message):
+     cmd  = message.chat.txt
+     await app.send_message(channel_id,"Updating....."+cmd.split("")[1])
+     os.system("""yt-dlp --downloader aria2c -I 1:2 -o '%(title)s.%(ext)s' --download-archive dllinks.txt -f '(mp4)[height=?240]' --write-thumbnail --embed-metadata """ + cmd.split("")[1])
      for  filename in os.listdir():
                print(filename)
                if filename.endswith(".mp4") :

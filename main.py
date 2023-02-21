41c6530f65256898026655d6c557cf54027d56e2
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-02-21 07:28:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ async def main():
      link = "https://www.pornhub.com/playlist/263313231"
      await app.send_message(-1001737315050,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}")
      #await app.send_message(-1001373543632,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}")
-     os.system(f"""yt-dlp -I 1:5 --downloader aria2c --download-archive dled.txt -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
+     os.system(f"""yt-dlp  --downloader aria2c --download-archive dled.txt -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
      for  filename in os.listdir():
       if filename.endswith(".mp4"):
        for db in datab:

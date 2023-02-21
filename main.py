84c1933cc48a642f0cedc62beb2ff8e265e9cea4
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-02-21 12:01:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,13 +32,12 @@ async def progress(current, total):
 @app.on_message(filters.text & filters.private)
 async def echo(client, message):
     link = message.text
-    os.system("""yt-dlp --downloader aria2c -o '%(title)s.%(ext)s' -f '(mp4)[height=?240]' --write-thumbnail --embed-metadata """ + link)
+    os.system("""yt-dlp --downloader aria2c -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
     for  filename in os.listdir():
-               print(filename)
                if filename.endswith(".mp4")  :
-                    await app.send_video(-1001737315050, video=filename,caption=filename,thumb=filename,progress=progress)
+                    await app.send_video(-1001737315050, video=filename,caption=filename,thumb=filename.replace(".mp4",".jpg"),progress=progress)
                     os.system(f"""rclone --config "./rclone.conf" move '{filename}' "Drive:/{crtda2}_Videos/" """)
-                    os.system(f"""rclone --config "./rclone.conf" move "Drive:/" "TD:/PH/" -vP --drive-server-side-across-configs=true """)
+                    os.system(f"""rclone --config "./rclone.conf" move "Drive:/{crtda2}_Videos/" "TD:/PH/" -vP --drive-server-side-across-configs=true """)
 
 
 
@@ -49,19 +48,18 @@ async def echo(client, message):
 async def start_command(client,message):
      cmd = message.text
      channel_id = message.chat.id
-     uph = await message.reply("Updating.....")
+     uph = await message.reply(f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/ForceBackups/{crtda2}/")
+
      filec = open("links.txt","r")
      read=csv.reader(filec)
      for link in read:
-        os.system(f"""yt-dlp --downloader aria2c -I 1:{cmd.split()[1]} -o '%(title)s.%(ext)s' --download-archive dled.txt -f '(mp4)[height=?240]' --write-thumbnail --embed-metadata """ + link[0])
-        #await app.edit_message_text(channel_id, uph.msg.id,"Uploading.....")
-        
+        os.system(f"""yt-dlp --downloader aria2c -I 1:5 -o '%(title)s.%(ext)s' --download-archive dled.txt -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link[0])
         for  filename in os.listdir():
                if filename.endswith(".mp4"):
                     print(filename)
-                    await app.send_video(-1001737315050, video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"),progress=progress)
+                    #await app.send_video(-1001737315050, video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"),progress=progress)
                     os.system(f"""rclone --config "./rclone.conf" move '{filename}' "Drive:/{crtda2}/" """)
-                    os.system(f"""rclone --config "./rclone.conf" move "Drive:/" "TD:/Backup/ForceBackups/" -vP --drive-server-side-across-configs=true """)
+                    os.system(f"""rclone --config "./rclone.conf" move "Drive:/{crtda2}/" "TD:/Backup/ForceBackups/" -vP --drive-server-side-across-configs=true """)
 
 
 
@@ -71,41 +69,33 @@ async def start_command(client,message):
 async def start_command(client,message):
      cmd  = message.text
      channel_id = message.chat.id
-     await app.send_message(channel_id,f"Downloading 10 Videos of.:\n\
-{cmd.split()[1]}")
+     if "playlist" in cmd.split()[1]:
+        await app.send_message(channel_id,f"Downloading 10 Videos of Playlist:\n{cmd.split()[1]}")
+     else:
+         await app.send_message(channel_id,f"Downloading:\n{cmd.split()[1]}")
      os.system("""yt-dlp --downloader aria2c -I 10 -o '%(title)s.%(ext)s' --download-archive dled.txt -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + cmd.split()[1])
      for  filename in os.listdir():
                print(filename)
                if filename.endswith(".mp4") :
                     await app.send_video(-1001737315050, video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"),progress=progress)
                     os.system(f"""rclone --config "./rclone.conf" move '{filename}' "Drive:{crtda2}_Videos/" """)
-                    os.system(f"""rclone --config "./rclone.conf" move "Drive:/" "TD:/PH/" -vP --drive-server-side-across-configs=true """)
-     await app.stop()
+                    os.system(f"""rclone --config "./rclone.conf" move "Drive:{crtda2}_Videos/" "TD:/PH/" -vP --drive-server-side-across-configs=true """)
             
     
 
 async def main():
    async with app:
-     datab = read_db()
-     print(datab)
      link = "https://www.pornhub.com/playlist/263313231"
      await app.send_message(-1001737315050,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}/")
      #await app.send_message(-1001373543632,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}/")
-     os.system(f"""yt-dlp  --downloader aria2c  -I 1:2 --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
+     os.system(f"""yt-dlp  --downloader aria2c  --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
      for  filename in os.listdir():
       if filename.endswith(".mp4"):
-       await app.send_video(-1001737315050, video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"))
-       await app.send_video(-1001737315050, video=filename,caption=filename.replace(".mp4",""))
-       for db in datab:
-            if filename in db:
-              break
-       else:
-            insert_db(filename)
             await app.send_video(-1001737315050, video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"),progress=progress)
             #await app.send_photo(-1001737315050, photo=filename.replace(".mp4",".jpg"),caption=f"{filename}")                    
             os.system(f"""rclone --config "./rclone.conf" move '{filename}' "Drive:{crtda2}/" """)
             os.system(f"""rclone --config "./rclone.conf" move "{filename}" "Db:/PH-Pictures/" """)
-            os.system(f"""rclone --config "./rclone.conf" move "Drive:" "TD:Backup/" -vP --drive-server-side-across-configs=true """)
+            os.system(f"""rclone --config "./rclone.conf" move "Drive:{crtda2}/" "TD:Backup/" -vP --drive-server-side-across-configs=true """)
             try:
               os.remove(filename)
             except:

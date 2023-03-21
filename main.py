23d893bda165f75908bc5fe28a0baa24b6977a6c
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-03-21 13:05:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def start_command(client,message):
 async def main():
    async with app:
      link = "https://www.pornhub.com/playlist/263313231"
-     status = await app.send_message(-1001737315050,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}/")
+     status = await app.send_message(-1001737315050,f"Update Started!\nDate:{crtda}")
      #await app.send_message(-1001373543632,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}/")
      os.system(f"""yt-dlp   --downloader aria2c  --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
      #os.system(f"""yt-dlp   --downloader aria2c  --skip-download  -o '%(title)s.%(ext)s'  --write-thumbnail --embed-metadata """ + link)

e057548a03493fd2eea1bce149b145599e802d25
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-03-21 16:14:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ app = Client(
 )
 
 tglinks=""
+tglink= tuple(tglinks)
 def write (text):
    global tglinks
    tglinks+=text
@@ -94,7 +95,7 @@ async def main():
      status = await app.send_message(-1001737315050,f"Update Started!\nDate:{crtda}",reply_to_message_id=3391)
      #await app.send_message(-1001373543632,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}/")
      #os.system(f"""yt-dlp   --downloader aria2c  --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
-     os.system(f"""yt-dlp   --downloader aria2c -I 1:2  --skip-download  -o '%(title)s.%(ext)s'  --write-thumbnail --embed-metadata """ + link)
+     os.system(f"""yt-dlp   --downloader aria2c -I 1:3 --skip-download  -o '%(title)s.%(ext)s'  --write-thumbnail --embed-metadata """ + link)
      for  filename in os.listdir():
       if filename.endswith(".jpg"):
             #video = await app.send_video(-1001737315050, video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"),progress=progress)
@@ -108,7 +109,7 @@ async def main():
               os.remove(filename)
             except:
                print("File Moved I guess!!!")        
-     await app.send_message(-1001737315050,f"Update Completed Successfully...\ntuple({tglinks})", reply_to_message_id=status.id)      
+     await app.send_message(-1001737315050,f"Update Completed Successfully...\n{tglink})", reply_to_message_id=status.id,parse_mode=enums.ParseMode.MARKDOWN)      
 
 
 app.run(main())

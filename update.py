e448809a1294511c3cf41decc4d93c4a22af9b8d
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-03-22 14:51:46 +0530

    Update update.py

--- update.py
+++ update.py
@@ -43,7 +43,7 @@ async def start_command(client,message):
     for  filename in os.listdir():
                if filename.endswith(".mp4")  :
                     video = await app.send_video(-1001585702100,video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"))
-                    await app.send_photo(-100951605329, photo=filename.replace(".mp4",".jpg"), caption=f"https://t.me/1585702100/{video.id}") 
+                    await app.send_photo(-1001945634929, photo=filename.replace(".mp4",".jpg"), caption=f"https://t.me/c/1585702100/{video.id}") 
                     os.system(f'''rclone --config './rclone.conf' move """{filename.replace('.mp4','.jpg')}"""  'PH_Pics:/Pictures/Custom/{link.split('/')[-1]}'  ''')
                     #os.system(f'''rclone --config './rclone.conf' move  """{filename}"""  'Drive:/'  ''')
                     #os.system(f"""rclone --config './rclone.conf' move "Drive:/" "TD:/" -vP --delete-empty-src-dirs --drive-server-side-across-configs=true """)

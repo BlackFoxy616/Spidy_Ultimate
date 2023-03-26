7d8d1fd8b1526da8e2e72c1de37bc2a64277d51e
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-03-26 21:19:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -93,8 +93,8 @@ async def main():
      link = "https://www.pornhub.com/playlist/263313231"
      status = await app.send_message(-1001737315050,f"Update Started!\nDate:{crtda}")
      #await app.send_message(-1001373543632,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}/")
-     os.system(f"""yt-dlp   --downloader aria2c  --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --embed-thumbnail --embed-metadata """ + link)
-     #os.system(f"""./yt-dlp   --downloader aria2c -I 1:1 -o '%(title)s.%(ext)s' -f '(mp4)[height=?240]' --write-thumbnail --embed-metadata """ + link)
+     os.system(f"""yt-dlp   --downloader aria2c -I 1:100 --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?720]' --embed-thumbnail --embed-metadata """ + link)
+     #os.system(f"""./yt-dlp   --downloader aria2c -I 1:1 -o '%(title)s.%(ext)s' -f '(mp4)[height=?720]' --write-thumbnail --embed-metadata """ + link)
      for  filename in os.listdir():
       if filename.endswith(".mp4"):
             os.system(f'''vcsi """{filename}""" -o """{filename.replace('.mp4','.png')}""" ''')

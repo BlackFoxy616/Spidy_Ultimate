094d11a752b8dea709d9c2a62b22513307812572
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-03-26 06:11:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -93,23 +93,28 @@ async def main():
      link = "https://www.pornhub.com/playlist/263313231"
      status = await app.send_message(-1001737315050,f"Update Started!\nDate:{crtda}")
      #await app.send_message(-1001373543632,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}/")
-     #os.system(f"""yt-dlp   --downloader aria2c  --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
-     os.system(f"""yt-dlp   --downloader aria2c -I 1:2 -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
+     os.system(f"""yt-dlp   --downloader aria2c  --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --embed-thumbnail --embed-metadata """ + link)
+     #os.system(f"""./yt-dlp   --downloader aria2c -I 1:1 -o '%(title)s.%(ext)s' -f '(mp4)[height=?240]' --write-thumbnail --embed-metadata """ + link)
      for  filename in os.listdir():
       if filename.endswith(".mp4"):
+            os.system(f'''vcsi """{filename}""" -o """{filename.replace('.mp4','.png')}""" ''')
             video = await app.send_video(-1001737315050, video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"),progress=progress)
-            pic = await app.send_photo(-1001585702100, photo=filename,caption=f"{filename}\n("[Video](f"https://t.me/c/1737315050/{video.id}"),"),parse_mode=enums.ParseMode.MARKDOWN)   
-            
+            vid = f"https://t.me/c/1737315050/{video.id}"
+            pic = await app.send_photo(-1001945634929, photo=filename.replace(".mp4",".png"),caption=vid)   
             os.system(f'''rclone --config './rclone.conf' move """{filename.replace('.mp4','.jpg')}"""  'PH_Pics:/Pictures/'  ''')
-            #os.system(f'''rclone --config './rclone.conf' move """{filename}"""  'PH_Pics:/Pictures/'  ''')
+            os.system(f'''rclone --config './rclone.conf' move """{filename.replace('.mp4','.png')}"""  'PH_Pics:/Pictures/Caps/'  ''')
             os.system(f'''rclone --config './rclone.conf' move  """{filename.replace('.mp4','.jpg')}"""  'Drive:/Pictures/'  ''')
+            os.system(f'''rclone --config './rclone.conf' move  """{filename.replace('.mp4','.png')}"""  'Drive:/Pictures/Caps'  ''')
+            #os.system(f'''rclone --config './rclone.conf' move """{filename}"""  'PH_Pics:/Pictures/'  ''')
             #os.system(f'''rclone --config './rclone.conf' move  """{filename}"""  'Drive:/Backup/'  ''')
             #os.system(f"""rclone --config './rclone.conf' move "Drive:/Backup/" "TD:Backup/" -vP --delete-empty-src-dirs --drive-server-side-across-configs=true """)
-            try: 
+            try:
+              os.remove(filename.replace(".mp4",".jpg"))
+              os.remove(filename.replace(".mp4",".png"))) 
               os.remove(filename)
             except:
                print("File Moved I guess!!!")        
-     await app.send_message(-1001737315050,f"Update Completed Successfully...)", reply_to_message_id=status.id)      
+     await app.send_message(-1001737315050,f"Update Completed Successfully...)", reply_to_message_id=status.id)
 
 
 app.run(main())

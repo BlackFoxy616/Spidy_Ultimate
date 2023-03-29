f7fc4f9947ac08020a77436da6c70e61a7274ccd
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-03-29 06:10:00 +0530

    Update update.py

--- update.py
+++ update.py
@@ -83,7 +83,7 @@ async def start_command(client,message):
     os.system("""yt-dlp --downloader aria2c  -I 1:15 --download-archive dl.txt -o '%(title)s.%(ext)s' -f '(mp4)[height=?720]' --write-thumbnail --embed-metadata """ + link)
     for  filename in os.listdir():
                if filename.endswith(".mp4") :
-                    os.system(f'''vcsi """{filename}""" -g 3x6 --metadata-position hidden -o """{filename.replace('.mp4','.png')}""" ''')
+                    os.system(f'''vcsi """{filename}""" -g 2x6 --metadata-position hidden -o """{filename.replace('.mp4','.png')}""" ''')
                     video = await app.send_video(-1001585702100,video=filename,caption=filename.replace(".mp4",""),thumb=filename.replace(".mp4",".jpg"))
                     await app.send_photo(-1001945634929, photo=filename.replace(".mp4",".png")) 
                     os.system(f'''rclone --config './rclone.conf' move """{filename.replace('.mp4','.jpg')}"""  'PH_Pics:/Pictures/Custom/{link.split('/')[-1]}'  ''')

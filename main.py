401589813620baf98929439e9ed3013b4639a316
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-04-02 12:31:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def main():
      link = "https://www.pornhub.com/playlist/263313231"
      #status = await app.send_message(-1001737315050,f"Update Started!\nDate:{crtda}")
      #await app.send_message(-1001373543632,f"Update Started!\nDate:{crtda}\nIndex Link: {indexlink}/Backup/{crtda2}/")
-     os.system(f"""yt-dlp   --downloader aria2c -I 800:1000 --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
+     os.system(f"""yt-dlp   --downloader aria2c -I  --download-archive dled.txt  -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
      #os.system(f"""./yt-dlp   --downloader aria2c -I 700:701 -o '%(title)s.%(ext)s' -f '(mp4)[height=?480]' --write-thumbnail --embed-metadata """ + link)
      for  filename in os.listdir():
       if filename.endswith(".mp4"):
@@ -63,8 +63,6 @@ async def main():
             pic = await app.send_photo(-1001945634929, photo=filename.replace(".mp4",".png"))   
             os.system(f'''rclone --config './rclone.conf' move """{filename.replace('.mp4','.jpg')}"""  'PH_Pics:/Pictures/'  ''')
             os.system(f'''rclone --config './rclone.conf' move """{filename.replace('.mp4','.png')}"""  'PH_Pics:/Pictures/Caps/'  ''')
-            os.system(f'''rclone --config './rclone.conf' move  """{filename.replace('.mp4','.jpg')}"""  'Drive:/Pictures/'  ''')
-            os.system(f'''rclone --config './rclone.conf' move  """{filename.replace('.mp4','.png')}"""  'Drive:/Pictures/Caps'  ''')
             #os.system(f'''rclone --config './rclone.conf' move """{filename}"""  'PH_Pics:/Pictures/'  ''')
             os.system(f'''rclone --config './rclone.conf' move  """{filename}"""  'Drive:/Backup/'  ''')
             os.system(f"""rclone --config './rclone.conf' move "Drive:/Backup/" "TD:Backup/" -vP --delete-empty-src-dirs --drive-server-side-across-configs=true """)
@@ -73,7 +71,8 @@ async def main():
               os.remove(filename.replace(".mp4",".png"))
               os.remove(filename)
             except:
-               print("File Moved I guess!!!")        
+               print("File Moved I guess!!!")     
+     os.system(f'''rclone --config './rclone.conf' copy  """PH_Pictures:/"""  'Drive:/Pictures/'  --drive-server-side-across-configs=true ''')
      #await app.send_message(-1001737315050,f"Update Completed Successfully...", reply_to_message_id=status.id)
      await app.edit_message_text(-1001984459303,4,text=stats("Offline",crtda,count))
 

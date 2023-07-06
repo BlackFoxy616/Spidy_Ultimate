d8b798eb3539123d0009108fdf5482008b584d38
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-07-06 09:27:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ app = Client(
 @app.on_message(filters.command("kh"))
 async def start_command(client,message):
          mess= message.text[4:]
-         print(mess)
+         await app.delete_messages(message.chat.id,message.id)
+         #print(mess)
          cdid= message.chat.id
          button_list =[]
          for each in kidl(mess):
@@ -32,6 +33,9 @@ async def start_command(client,message):
 
 @app.on_callback_query()
 async def answer(client, call):
+         chat = -1001502224148
+         #print(chat)
+         await app.delete_messages(chat,call.message.id)
          data = ddd(call.data.split("_")[1])
          for url in data:
           name = call.data.split("_")[0]
@@ -47,21 +51,34 @@ async def answer(client, call):
                st = "http:"+sturl
     
           #await app.send_message(call.message.chat.id,st)
-          title = f"{name}-Ep-{int(data.index(url))+1}old"
-          os.system(f"yt-dlp --downloader aria2c -o '{title}.%(ext)s' {st}")
-          os.system(f'''aria2c '{sub}' -o "{title}.{sub.split(".")[-1]}" ''')
-          for file in os.listdir():
-              if title in file and not (file.endswith("srt")) :
-                 os.system(f'''ffmpeg -i {file} -i {file.replace(file.split(".")[-1],"srt")} -c copy -c:s mov_text language=eng {file.replace("old","")}''')
-                 os.system(f'''vcsi """{file}""" -g 2x6 --metadata-position hidden -o """{file.replace(file.split(".")[-1],'.png')}""" ''')
-                 await send_video(call.chat.id,file.replace("old",""))
-                 await app.send_photo(call.chat.id, photo=file.replace(file.split(".")[-1],".png"))
-                 try:
-                  os.remove(file)
-                 except:
-                    pass
-                 
+
+
+
+
+          filename = f"{name}-Ep-{int(data.index(url))+1}"
+          os.system(f"""./yt-dlp --downloader aria2c -o '{filename}.%(ext)s' {st}""")
+          #os.system(f'''aria2c '{sub}' -o "{filename}.{sub.split(".")[-1]}" ''')
+          for title in os.listdir():
+            if filename in title and ("mkv" in title or "mp4" in title):
+              os.system(f'''vcsi """{title}""" -g 2x6 --metadata-position hidden -o """{title.replace(title.split(".")[-1],"png")}""" ''')
+              await app.send_video(chat,f"{title}",thumb=f"""{title.replace(title.split(".")[-1],"png")}""")
+              
+            elif filename in title:
+                 print(filename)
+                 await app.send_document(chat,filename)  
+
+
+
+         
+            
 print("Bot Started")
 app.run()
 
 
+#if len(sub) > 0:
+                
+                #os.system(f'''ffmpeg -y -i "{title}" -i "{title.replace(title.split(".")[-1],sub.split(".")[-1])}" -c copy -c:s mov_text  "{title.replace(title.split(".")[-1],"en"+title.split(".")[-1])}"''')
+ #               os.system(f"rm {title}")
+#              os.system(f"""mv "{title.replace(title.split(".")[-1],"en"+title.split(".")[-1])}" "{title}"  """)
+                
+              

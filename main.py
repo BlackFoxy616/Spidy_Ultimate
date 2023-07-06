164bfedaae2707df023bf55563e75ce9ccc10152
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-07-06 06:52:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+from pyrogram import Client,filters
+from kidl import *
+import yt_dlp,os
+from pyrogram.types import (ReplyKeyboardMarkup, InlineKeyboardMarkup,
+                            InlineKeyboardButton)
+
+# Create a client using your bot token
+api_id = 3702208
+api_hash = "3ee1acb7c7622166cf06bb38a19698a9"
+bot_token = "5300188722:AAFlruACp00Hv2ZD1RPjE9P0FahI52swqpU"
+
+app = Client(
+    "my_bot",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
+)
+
+
+
+
+@app.on_message(filters.command("kh"))
+async def start_command(client,message):
+         mess= message.text[4:]
+         print(mess)
+         cdid= message.chat.id
+         button_list =[]
+         for each in kidl(mess):
+             button_list.append([InlineKeyboardButton(each['title'], callback_data =str(each['title'])+"_"+str(each['id']))]) 
+             reply_markup=InlineKeyboardMarkup(button_list)
+         await app.send_message(
+            cdid,"Select The Required Drama:",reply_markup=reply_markup)
+
+@app.on_callback_query()
+async def answer(client, call):
+         data = ddd(call.data.split("_")[1])
+         for url in data:
+          name = call.data.split("_")[0]
+          id = str(url['id'])
+          URL = base_url + "DramaList/Episode/"+id+".png?err=false&ts=&time="
+          sub = get_subtitles(id)
+          if len(sub) > 0:
+               sub = get_subtitles(id)[0]
+          sturl= requests.get(URL).json()['Video']
+          if sturl.startswith("http") or sturl.startswith("https"):
+                   st=sturl
+          else:
+               st = "http:"+sturl
+    
+          await app.send_message(call.message.chat.id,st)
+          title = f"{name}-Ep-{int(data.index(url))+1}old"
+          print(f"ytdlp --downloader aria2c -o '{title}.%(ext)s'")
+          print(f'''aria2c '{sub}' -o "{title}.{sub.split(".")[-1]}" ''')
+          for file in os.listdir():
+              if title in file and not (file.endswith("srt")) :
+                 print(f'''ffmpeg -i {file} -i {file.replace(file.split(".")[-1],"srt")} -c copy -c:s mov_text language=eng {file.replace("old","")}''')
+
+      
+print("Bot Started")
+app.run()
+
+
---
+++ sub.py
@@ -0,0 +1,110 @@
+import requests
+import yt_dlp
+
+
+base_url = "https://kisskh.co/api/"
+
+def kissytdl(link):
+   options = {
+  "quiet":    True,
+  "simulate": True,
+  "forceurl": True,
+  }
+   with yt_dlp.YoutubeDL(options) as ytdl:
+    ytdl.download(link)
+       
+
+def build_menu(buttons,n_cols,header_buttons=None,footer_buttons=None):
+  menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+  if header_buttons:
+    menu.insert(0, header_buttons)
+  if footer_buttons:
+    menu.append(footer_buttons)
+  return menu
+ 
+
+
+def kidl(query):
+    URL = base_url + "DramaList/Search?q=" + query
+    ks = requests.get(url=URL)
+    data1 = ks.json()
+    return data1
+
+
+
+def did(query):
+     json = kidl(query)
+     for i in range(len(json)):
+       title=json[i]['title']
+       print(i+1,title)
+     c = int(input("Select Wanted Drama:"))
+     id=json[c-1]['id']
+     return id
+
+def dd(query):
+   id = str(did(query))
+   url = base_url + "DramaList/Drama/" +id
+   data = requests.get(url)
+   json= data.json()['episodes'][::-1]
+   return json
+
+def ddd(id):
+   url = base_url + "DramaList/Drama/" +id
+   data = requests.get(url)
+   json= data.json()['episodes'][::-1]
+   return json
+
+def steam_url_json(query):
+   ids = []
+   json = dd(query)
+   print (json)
+   print(len(json))
+   return
+    
+def  steam_url(query):
+    for i in dd(query):
+     id = str(i['id'])
+     url = base_url + "DramaList/Episode/"+id+".png?err=false&ts=&time="
+     steam_json= requests.get(url).json()
+     print("https:" + steam_json['Video'])
+
+def  steam_url(id):
+    for i in ddd(id):
+     id = str(i['id'])
+     url = base_url + "DramaList/Episode/"+id+".png?err=false&ts=&time="
+     steam_url= "https:" + requests.get(url).json()
+    return steam_url
+
+def kissdl(query):
+  kissytdl(steam_url(query))
+
+
+
+
+
+
+def get_subtitles(episode_id ):
+        subtitle_api_url = f"{base_url}Sub/{episode_id}"
+        response = requests.get(subtitle_api_url)
+        res = response.json()
+        subs =[]
+        for sub in res:
+           if "en" in sub["land"]:
+                subs.append(sub["src"])
+        return subs
+                
+        
+
+
+#search = input('Enter A Drama Name:')
+#steam_url("Wednesday")
+"""
+key = []
+info = kidl("Demon Slayer")
+for i in range(len(info)):
+     title = info[i]['title']
+     btn = "InlineKeyboardButton(" + title + ",callback_data="+str(i)+")"
+     key.append([btn])
+print(key)
+
+"""

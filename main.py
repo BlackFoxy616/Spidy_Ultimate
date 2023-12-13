c91d017d1c25112c25ce44042005921b650327d3
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-12-13 23:49:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 
 
 files = [file  for file in os.listdir("Scripts")]
+count = 0
 for sp in files:
     print(f"Running{sp}")
-    os.system(f"nohup python {sp} &")
+    if sp.endswith(".mp4"):
+        os.system(f"nohup python {sp} &")
+    if count == len(files):
+        os.system(f"python {sp}")
+    count+=1

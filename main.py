772e8101ce9daec1cbb34df16b35e7f81253cb46
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-12-13 23:41:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,4 @@ import os
 files = [file  for file in os.listdir("Scripts")]
 for sp in files:
     print(f"Running{sp}")
-    if list.index(sp) == len(files)-1:
-          os.system(f"python {sp} ")
-    else:
-      os.system(f"nohup python {sp} &")
+    os.system(f"nohup python {sp} &")
